a8bdd03ccca7d7abeb9f45d7d170245d1c571bb3
Semen Syrovatskiy <mail@syrovatskiy.tk>
2022-10-26 01:06:22 +0300

    refactor

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,6 +1,6 @@
 use poem::{
-    http::header::AUTHORIZATION,
-    test::{TestClient as PoemTestClient, TestRequestBuilder},
+    http::{header::AUTHORIZATION, StatusCode},
+    test::{TestClient as PoemTestClient, TestRequestBuilder, TestResponse},
 };
 use serde_json::{json, Value as JsonValue};
 use sqlx::SqlitePool;
@@ -27,31 +27,49 @@ impl TestClient {
             .body_json(&json!({"username": username, "password": password}))
             .send()
             .await;
+        self.extract_token(resp).await;
+    }
+
+    async fn extract_token(&mut self, resp: TestResponse) {
+        resp.assert_status(StatusCode::CREATED);
         let resp: JsonValue = resp.json().await.value().deserialize();
-        self.token = Some(resp["result"]["token"].as_str().unwrap().to_string());
+        self.token = Some(
+            resp["result"]["token"]
+                .as_str()
+                .expect("Failed to extract access token.")
+                .to_owned(),
+        );
+    }
+
+    fn add_auth_header<'a>(
+        &'a self,
+        req: TestRequestBuilder<'a, app::App>,
+    ) -> TestRequestBuilder<'a, app::App> {
+        if self.token.is_some() {
+            req.header(
+                AUTHORIZATION,
+                format!("Bearer {}", self.token.clone().unwrap()),
+            )
+        } else {
+            req
+        }
     }
 
     pub fn get(&self, uri: &str) -> TestRequestBuilder<app::App> {
         let req = self.client.get(uri);
-        if self.token.is_some() {
-            req.header(
-                AUTHORIZATION,
-                format!("Bearer {}", self.token.clone().unwrap()),
-            )
-        } else {
-            req
-        }
+        let req = self.add_auth_header(req);
+        req
     }
 
     pub fn post(&self, uri: &str, body: &JsonValue) -> TestRequestBuilder<app::App> {
         let req = self.client.post(uri).body_json(body);
-        if self.token.is_some() {
-            req.header(
-                AUTHORIZATION,
-                format!("Bearer {}", self.token.clone().unwrap()),
-            )
-        } else {
-            req
-        }
+        let req = self.add_auth_header(req);
+        req
+    }
+
+    pub fn patch(&self, uri: &str, body: &JsonValue) -> TestRequestBuilder<app::App> {
+        let req = self.client.patch(uri).body_json(body);
+        let req = self.add_auth_header(req);
+        req
     }
 }
